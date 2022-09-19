a98809793d150aaa31928dda4016ae0b6ffd6d02
Mayukh <mayukhgautam@cmail.carleton.ca>
2022-09-19 02:04:05 -0400

    Adds a BuddyInfo to a list and removes it

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class AddressBook {
 
+    List<BuddyInfo> list;
+
+    public AddressBook(){
+        list = new LinkedList<>();
+    }
+
+    public void add(BuddyInfo bi){
+        list.add(bi);
+    }
+
+    public void remove(BuddyInfo bi){
+        list.remove(bi);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddyInfo = new BuddyInfo();
+        AddressBook aBook = new AddressBook();
+        aBook.add(buddyInfo);
+        aBook.remove(buddyInfo);
     }
 }
